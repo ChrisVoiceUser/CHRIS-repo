0a16bc1cfdae467cc1b242c4da06f2574aa57ccd
ChrisVoiceUser <XelectroporationX@gmail.com>
2013-07-01 15:59:04 -0400

    user can now officially exit by sayind "Done"

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ from CHRIS import * #imports everything from CHRIS.py
 USER_DATA=initializeUserData() #user credentials
 listener=Listener(USER_DATA[2]) #initializes listener so we can use its methods
 user=User(USER_DATA[1]) #here for future purposes
-while True:#keep looping until user if finished
+
+while True:#infinite loop
+    speech.say('you can exit by saying, done')
     listener.askCurrentCommand()
-    listener.executeCurrentCommand()
-    
+    if listener.getCurrentCommand()=='Dawn' or listener.getCurrentCommand()=='Don' or listener.getCurrentCommand()=='Done': #user says 'done' to end
+        speech.say('goodbye')
+        break #exits the infinite loop
+    else: #if the current command is something other than done, that means it is an actual command
+        listener.executeCurrentCommand()
+
+  
 
